fbf291f4d2aca25f63abb1c6ce6135675e57b4b9
amirizhar <amir_izhar@yahoo.com>
2023-02-01 19:57:55 +0800

    contactPage

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -1,49 +1,53 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>
-  Login
-</title>
+    <title>
+        Book Centre | Login
+    </title>
 
-@include('Login.loginCss')
-@include('Login.loginScript')
+    @include('Login.loginCss')
+    @include('Login.loginScript')
 
 </head>
 
 <body id="particles-js"></body>
 <div class="animated bounceInDown">
-  <div class="container">
-    <span class="error animated tada" id="msg"></span>
-    <form action="/login" method="post"  class="box">
-      @csrf
-      <h4>Admin<span>Dashboard</span></h4>
-      <h5>Sign in to your account.</h5>
+    <div class="container">
+        <span class="error animated tada" id="msg"></span>
+        <form action="/login" method="post" class="box">
+            @csrf
+            <h4><span>Book Centre</span></h4>
+            <h5>Sign in to your account.</h5>
 
-        <input type="email" name="email" placeholder="Email">
-        <i class="typcn typcn-eye" id="eye"></i>
+            <input type="email" name="email" placeholder="Email">
+            <i class="typcn typcn-eye" id="eye"></i>
 
-        <input type="password" name="password" placeholder="Passsword" id="pwd">
-        <label>
-          <input type="checkbox">
-          <span></span>
-          <small class="rmb">Remember me</small>
-        </label>
+            <input type="password" name="password" placeholder="Passsword" id="pwd">
+            <label>
+                <input type="checkbox">
+                <span></span>
+                <small class="rmb">Remember me</small>
+            </label>
 
-        {{-- <a href="#" class="forgetpass">Forget Password?</a> --}}
+            {{-- <a href="#" class="forgetpass">Forget Password?</a> --}}
 
-        <input type="submit" value="Sign in" class="btn1">
-      </form>
+            <input type="submit" value="Sign in" class="btn1">
+        </form>
 
         {{-- <a href="#" class="dnthave">Don’t have an account? Sign up</a> --}}
-  </div> 
+    </div>
 
-  {{-- --------------------footer ----------------------}}
-       <div class="footer">
-      <span>Made with <i class="fa fa-heart pulse"></i> <a href="https://www.google.de/maps/place/Augsburger+Puppenkiste/@48.360357,10.903245,17z/data=!3m1!4b1!4m2!3m1!1s0x479e98006610a511:0x73ac6b9f80c4048f"><a href="https://codepen.io/lordgamer2354">By Anees Khan</a></span>
+    {{-- --------------------footer -------------------- --}}
+    <div class="footer">
+        <span>Contact <br><i class="fa fa-heart pulse"></i> <a
+                href="https://www.google.de/maps/place/Augsburger+Puppenkiste/@48.360357,10.903245,17z/data=!3m1!4b1!4m2!3m1!1s0x479e98006610a511:0x73ac6b9f80c4048f"><a
+                    href="https://codepen.io/lordgamer2354">Admin of the system</a></span>
     </div>
 </div>
 
 </body>
+
 </html>
--- resources/views/Staff/contactPage.blade.php
+++ resources/views/Staff/contactPage.blade.php
@@ -48,16 +48,16 @@ if (Session::has('firebaseUserId') && Session::has('idToken'))
                                 <tr>
                                     {{-- <th scope="col">#</th> --}}
                                     <th scope="col">#</th>
-                                    <th scope="col">ISBN</th>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Language</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Messages</th>
                                     <th scope="col">Publisher</th>
-                                    <th scope="col">Price (RM)</th>
-                                    <th scope="col">Store</th>
+                                    <th scope="col">Date and Time</th>
+                                    {{-- <th scope="col">Store</th>
                                     <th scope="col">Category</th>
                                     <th scope="col">Stock</th>
-                                    <th scope="col">Summary</th>
-                                    <th scope="col">Action</th>
+                                    <th scope="col">Summary</th> --}}
+                                    {{-- <th scope="col">Action</th> --}}
                                 </tr>
                             </thead>
                             <tbody id="table-list">
@@ -65,237 +65,6 @@ if (Session::has('firebaseUserId') && Session::has('idToken'))
                             </tbody>
                         </table>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    {{-- create modal --}}
-    <div class="modal fade" id="add-modal" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Register Book</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <div class="modal-body">
-                    <form id="add-post" method="post">
-
-                        {{-- <div class="mb-3">
-                            <label for="cover" class="form-label">Choose a book cover:</label><br>
-                            <input type="file" id="cover" name="cover" accept="image/png, image/jpeg">
-                        </div> --}}
-
-                        <div class="mb-3">
-                            <label for="isbn" class="form-label">ISBN</label>
-                            <input type="number" class="form-control" id="isbn" name="isbn">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="title" class="form-label">Title</label>
-                            <input type="text" class="form-control" id="title" name="title">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="language" class="form-label">Language</label>
-                            <input type="text" class="form-control" id="isbn" name="language">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="publisher" class="form-label">Publisher</label>
-                            <input type="text" class="form-control" id="publisher" name="publisher">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="price" class="form-label">Price</label>
-                            <input type="number" class="form-control" id="price" name="price">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="store" class="form-label">Store</label>
-                            <input type="text" class="form-control" id="store" name="store">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="summary" class="form-label">Summary</label>
-                            <textarea type="text" class="form-control" id="summary" name="summary"></textarea>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="category" class="form-label">Caterory</label><br>
-                            <select type="text" name="category" id="category">
-                              <option value="Business & Economics">Business & Economics</option>
-                              <option value="Children's Books">Children's Books</option>
-                              <option value="Comics & Graphic">Comics & Graphic</option>
-                              <option value="Novels">Novels</option>
-                              <option value="Family & Relationships">Family & Relationships</option>
-                              <option value="Languages">Languages</option>
-                              <option value="Religion">Religion</option>
-                              <option value="Computers & Internet">Computers & Internet</option>
-                              <option value="Design & the Arts">Design & the Arts</option>
-                              <option value="History & Politics">History & Politics</option>
-                              <option value="Health & Well-Being">Health & Well-Being</option>
-                              <option value="Sport">Sport</option>
-                              <option value="Hobbies & Collectibles">Hobbies & Collectibles</option>
-                              <option value="Social Science">Social Science</option>
-                              <option value="Science">Science</option>
-                              <option value="Food & Drink">Food & Drink</option>
-                              <option value="Self-Enrichment">Self-Enrichment</option>
-                              <option value="Local study">Local study</option>
-                              <option value="Textbooks">Textbooks</option>
-                              <option value="Language Exam">Language Exam</option>
-                              <option value="International Exam">International Exam</option>
-                              <option value="Fiction">Fiction</option>
-                              <option value="Non-fiction">Non-fiction</option>
-                              <option value="Chinese Books">Chinese Books</option>
-                              <option value="Japanese Books">Japanese Books</option>
-                              <option value="Lifestyle">Lifestyle</option>
-                              <option value="Others">Others</option>
-                            </select>
-                            </div>
-
-                        <div class="mb-3">
-                        <label for="stock" class="form-label">Stock</label><br>
-                        <select type="text" name="stock" id="stock">
-                          <option value="Not Available">Not Available</option>
-                          <option value="Available">Available</option>
-                        </select>
-                        </div>
-
-                        <button type="button" id="add-submit" class="btn btn-primary">Register Book</button>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    {{-- update modal --}}
-    <div class="modal fade" id="update-modal" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Update Book</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <div class="modal-body">
-                    <form id="update-post" method="post">
-
-                        {{-- <div class="mb-3">
-                            <label for="update-title" class="form-label">Title</label>
-                            <input type="text" class="form-control" name="title" id="update-title">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="update-content" class="form-label">Content</label>
-                            <textarea class="form-control" name="content" id="update-content"></textarea>
-                        </div> --}}
-
-                        {{-- ------------------------------------------------------------------------------------ --}}
-
-
-                        {{-- <div class="mb-3">
-                            <label for="update-isbn" class="form-label">ISBN</label>
-                            <input type="number" class="form-control" id="update-isbn" name="isbn">
-                        </div> --}}
-
-                        <div class="mb-3">
-                            <label for="update-title" class="form-label">Title</label>
-                            <input type="text" class="form-control" id="update-title" name="title" disabled>
-                        </div> 
-
-                        {{-- <div class="mb-3">
-                            <label for="update-language" class="form-label">Language</label>
-                            <input type="text" class="form-control" id="update-language" name="language">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="update-publisher" class="form-label">Publisher</label>
-                            <input type="text" class="form-control" id="update-publisher" name="publisher">
-                        </div>  --}}
-
-                        <div class="mb-3">
-                            <label for="update-price" class="form-label">Price</label>
-                            <input type="number" class="form-control" id="update-price" name="price">
-                        </div>
-
-                        {{-- <div class="mb-3">
-                            <label for="update-store" class="form-label">Store</label>
-                            <input type="text" class="form-control" id="update-store" name="store">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="update-summary" class="form-label">Summary</label>
-                            <textarea type="text" class="form-control" id="update-summary" name="summary"></textarea>
-                        </div> --}}
-
-                        {{-- <div class="mb-3">
-                            <label for="update-category" class="form-label">Caterory</label><br>
-                            <select type="text" name="category" id="update-category">
-                              <option value="Business & Economics">Business & Economics</option>
-                              <option value="Children's Books">Children's Books</option>
-                              <option value="Comics & Graphic">Comics & Graphic</option>
-                              <option value="Novels">Novels</option>
-                              <option value="Family & Relationships">Family & Relationships</option>
-                              <option value="Languages">Languages</option>
-                              <option value="Religion">Religion</option>
-                              <option value="Computers & Internet">Computers & Internet</option>
-                              <option value="Design & the Arts">Design & the Arts</option>
-                              <option value="History & Politics">History & Politics</option>
-                              <option value="Health & Well-Being">Health & Well-Being</option>
-                              <option value="Sport">Sport</option>
-                              <option value="Hobbies & Collectibles">Hobbies & Collectibles</option>
-                              <option value="Social Science">Social Science</option>
-                              <option value="Science">Science</option>
-                              <option value="Food & Drink">Food & Drink</option>
-                              <option value="Self-Enrichment">Self-Enrichment</option>
-                              <option value="Local study">Local study</option>
-                              <option value="Textbooks">Textbooks</option>
-                              <option value="Language Exam">Language Exam</option>
-                              <option value="International Exam">International Exam</option>
-                              <option value="Fiction">Fiction</option>
-                              <option value="Non-fiction">Non-fiction</option>
-                              <option value="Chinese Books">Chinese Books</option>
-                              <option value="Japanese Books">Japanese Books</option>
-                              <option value="Lifestyle">Lifestyle</option>
-                              <option value="Others">Others</option>
-                            </select>
-                            </div> --}}
-
-                        <div class="mb-3">
-                        <label for="update-stock" class="form-label">Stock</label><br>
-                        <select type="text" name="stock" id="update-stock">
-                          <option value="Not Available">Not Available</option>
-                          <option value="Available">Available</option>
-                        </select>
-                        </div>
-
-                        <button type="button" id="update-button" class="btn btn-primary">Update</button>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    {{-- delete modal --}}
-    <div class="modal fade" id="delete-modal" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Delete post</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p class="lead">Are you sure you want to delete this book?</p>
-                    <input name="id" id="post-id" type="hidden">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" id="delete-button" class="btn btn-primary">Delete</button>
                 </div>
             </div>
         </div>
@@ -335,15 +104,8 @@ if (Session::has('firebaseUserId') && Session::has('idToken'))
                                 <td>' + value.language + '</td>\
                                 <td>' + value.publisher + '</td>\
                                 <td>' + value.price + '</td>\
-                                <td>' + value.store + '</td>\
-                                <td>' + value.category + '</td>\
-                                <td>' + value.stock + '</td>\
-                                <td style="text-align:justify;">' + value.summary +'</td>\
-                                <td><a data-bs-toggle="modal" data-bs-target="#update-modal" class="btn btn-success update-post" data-id="' +
-                        index +
-                        '">Update</a>\
-                                <a data-bs-toggle="modal" data-bs-target="#delete-modal" class="btn btn-danger delete-data" data-id="' +
-                        index + '">Delete</a></td>\
+                                <td>\
+                                </td>\
                             </tr>');
                 }
                 lastId = index;
@@ -372,63 +134,6 @@ if (Session::has('firebaseUserId') && Session::has('idToken'))
             lastId = createId;
             $("#add-post")[0].reset();
             $("#add-modal").modal('hide');
-        });
-
-        // -----------------------update modal-----------------------------
-        var updateID = 0;
-        $('body').on('click', '.update-post', function() {
-            updateID = $(this).attr('data-id');
-            firebase.database().ref('books/' + updateID).on('value', function(snapshot) {
-                var values = snapshot.val();
-                // $('#update-title').val(values.title);
-                // $('#update-isbn').val(values.isbn);
-                // $('#update-language').val(values.language);
-                // $('#update-publisher').val(values.publisher);
-                $('#update-price').val(values.price);
-                // $('#update-store').val(values.store);
-                // $('#update-summary').val(values.summary);
-                $('#update-stock').val(values.stock);
-                // $('#update-category').val(values.category);
-            });
-        });
-
-        // --------------------------update post----------------------------
-        $('#update-button').on('click', function() {
-            var values = $("#update-post").serializeArray();
-            var postData = {
-                // title: values[0].value,
-                // isbn: value[1].value,
-                // language: values[2].value,
-                // publisher: values[3].value,
-                price: values[0].value,
-                // store: values[5].value,
-                // summary: values[6].value,
-                stock: values[1].value,
-                // category: values[8].value,
-            };
-
-            var updatedPost = {};
-            updatedPost['/books/' + updateID] = postData;
-
-            firebase.database().ref().update(updatedPost);
-
-            $("#update-modal").modal('hide');
-            $("#update-post")[0].reset();
-        });
-
-        // ---------------------------delete modal-------------------------
-        $("body").on('click', '.delete-data', function() {
-            var id = $(this).attr('data-id');
-            $('#post-id').val(id);
-        });
-
-        // delete post
-        $('#delete-button').on('click', function() {
-            var id = $('#post-id').val();
-            firebase.database().ref('books/' + id).remove();
-
-            $('#post-id').val('');
-            $("#delete-modal").modal('hide');
         });
     </script>
 </body>
